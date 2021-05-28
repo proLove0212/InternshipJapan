b437bc8cbc70eefac58679765acb6cec234fcd0e
PROLOVE <proLove0212@gmail.com>
2021-05-28 17:57:33 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,25 @@
+<div class="footer">
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <h1>Lorem, ipsum dolor.</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h1>Lorem, ipsum dolor.</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h1>Lorem, ipsum dolor.</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem, ipsum dolor.</p>
+                <p><div class="img-credits"><a href="https://unsplash.com/@galen_crout" target="_blank"><i class="fas fa-camera-retro" style="margin-right: 8px;"></i>Galen Crout</a></div></p>
+            </div>
+        </div>
+    </div>
