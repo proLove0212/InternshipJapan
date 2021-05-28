f1a7d2873e688821227dc3ba06e3d7aaf9189dac
PROLOVE <proLove0212@gmail.com>
2021-05-28 17:49:18 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,167 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="style/stylesheet.css">
+
+    <!-- Bootstrap CSS -->
+    <script src="https://kit.fontawesome.com/5b23f4b030.js" crossorigin="anonymous"></script>
+
+    <!-- Font Krona -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet">
+
+    <!-- Font Open Sans -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
+    <title>Internship Japan</title>
+  </head>
+
+    <header>
+
+    <div class="landing">
+        <div class="landing-img">
+            <div class="landing-content">
+                <h1>Internship Japan</h1>
+                <div class="landing-search">
+                    <form action="">
+                        <div class="p-1 bg-light rounded rounded-pill shadow-sm mb-4">
+                            <div class="input-group">
+                                <input type="search" placeholder="Search for companies, internships.." aria-describedby="button-addon1" class="form-control border-0 bg-light">
+                                <div class="input-group-append">
+                                 <button id="button-addon1" type="submit" class="btn btn-link text-primary"><i class="fa fa-search"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </header>
+
+  <body>
+
+  <div class="container uneven-grid">
+    <div class="row">
+        <div class="col-12 col-md-10">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <div id="carousel" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img class="d-block w-100" src="images/gradient1.png" alt="First slide">
+                                <div class="carousel-caption">
+                                    <h5>Company Name</h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat exercitationem quo deserunt? Voluptatem ipsam, quis distinctio laudantium labore deleniti expedita.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="images/gradient2.png" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="images/gradient3.png" alt="Third slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                        </div>
+                    </div>
+                
+                <div class="col-12 col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">2 Lorem, ipsum dolor.</h5>
+                            <hr>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, libero!</p>
+                            <a href="#" class="btn btn-primary button">button</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">3 Lorem, ipsum dolor.</h5>
+                            <hr>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, libero!</p>
+                            <a href="#" class="btn btn-primary button">button</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+        <div class="col-12 col-md-2">
+            <div class="row">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="profilepicture">
+                            <a href="#"><img src="images/defaultprofilepicture.png" alt="profilepicture"></a>
+                        </div>
+                        <h5>Full Name</h5>
+                        <hr>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, libero!</p>
+                        <a href="#" class="btn btn-primary button">View my Account</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+    <div class="footer">
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <h1>Lorem, ipsum dolor.</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h1>Lorem, ipsum dolor.</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h1>Lorem, ipsum dolor.</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem, ipsum dolor.</p>
+                <p><div class="img-credits"><a href="https://unsplash.com/@galen_crout" target="_blank"><i class="fas fa-camera-retro" style="margin-right: 8px;"></i>Galen Crout</a></div></p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
---
+++ test.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="style/stylesheet.css">
+
+    <!-- Bootstrap CSS -->
+    <script src="https://kit.fontawesome.com/5b23f4b030.js" crossorigin="anonymous"></script>
+
+    <!-- Font Krona -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet">
+
+    <!-- Font Open Sans -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
+    <title>Internship Japan</title>
+  </head>
+
+  <body>
+
+    <div class="container uneven-grid">
+        <div class="row">
+            <div class="col-12 col-md-9">
+                <div class="banner">
+                    <div class="banner-profileimg">
+                     <img src="images/defaultprofilepicture.png" alt="">
+                    </div>
+                <h5 class="banner-name">Name Intern</h5>
+                </div>
+                <div class="account-content">
+                    <h5>Studies Software Development</h5>
+                    <h6>at ROC Nijmegen in The Netherlands</h6>
+                    <hr>
+                </div>
+            </div>
+        <div class="col-12 col-md-3">
+            <div class="card sidecard">
+
+            </div>
+        </div>
+        </div>
+        <div class="col-12 col-md-9">
+            <div class="card">
+                <div class="card-body">
+                    <div class="account-info">
+                        <h5>Info</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quaerat suscipit sint quasi et enim vel, ipsum minus, non mollitia, voluptatem qui quidem totam id impedit dolorem voluptatibus a sunt excepturi magnam molestiae? Adipisci magni perferendis ea. Tenetur, similique quam!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <h1>Lorem, ipsum dolor.</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h1>Lorem, ipsum dolor.</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem, ipsum dolor.</p>
+                <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h1>Lorem, ipsum dolor.</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem, ipsum dolor.</p>
+                <p><div class="img-credits"><a href="https://unsplash.com/@galen_crout" target="_blank"><i class="fas fa-camera-retro" style="margin-right: 8px;"></i>Galen Crout</a></div></p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
